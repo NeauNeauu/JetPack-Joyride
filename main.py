2c9985d80826cbcb463fb6745c054aa4d7f993f7
NeauNeauu <noah.ney@epitech.eu>
2024-06-07 09:51:36 +0200

    [ADD] pause menu

--- main.py
+++ main.py
@@ -16,12 +16,21 @@ screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Jetpack Joyride")
 
 player_sprite = pygame.image.load("assets/player.png")
+
+# Background layers
 background_layers = [
     pygame.image.load("assets/background.jpg"),
     pygame.image.load("assets/background.jpg"),
     pygame.image.load("assets/background.jpg")
 ]
 laser_sprite = pygame.image.load("assets/laser.png")
+background_menu = pygame.image.load("assets/background_menu.png")
+background_pause = pygame.image.load("assets/background_pause.png")
+
+background_layers = [pygame.transform.scale(layer, (1920, 1080)) for layer in background_layers]
+background_menu = pygame.transform.scale(background_menu, (1920, 1080))
+background_pause = pygame.transform.scale(background_pause, (1920, 1080))
+
 
 # Player sprite sheet frames management
 SPRITE_WIDTH = 135
@@ -44,7 +53,6 @@ player_velocity = [0]
 current_frame = [0]
 frame_count = [0]
 
-background_layers = [pygame.transform.scale(layer, (1920, 1080)) for layer in background_layers]
 
 # Initial background layer speeds
 layer_speeds = [4, 5, 6]
@@ -63,6 +71,7 @@ laser_speed = [5]
 score = [0]
 font = pygame.font.Font("assets/font.ttf", 40)
 
+
 def create_laser():
     x_pos = random.randint(1920, 3840)
     y_pos = random.randint(180, 900)
@@ -78,6 +87,7 @@ def create_laser():
 
     return (laser, laser_rect, laser_mask)
 
+
 def reset_game():
     global player_rect, player_velocity, score, lasers, layer_speeds, laser_speed
     player_rect.topleft = (500, 1020)
@@ -87,6 +97,7 @@ def reset_game():
     layer_speeds = [4, 5, 6]
     laser_speed[0] = 5
     LASER_INTERVAL[0] = 2000
+
 
 def display(player_rect, frames_ground, frames_air, background_layers, layer_positions, lasers, score):
     screen.fill((0, 0, 0))
@@ -109,6 +120,7 @@ def display(player_rect, frames_ground, frames_air, background_layers, layer_pos
     pygame.display.flip()
     clock.tick(FPS)
 
+
 class Button:
     def __init__(self, text, x, y, width, height, color, hover_color, action):
         self.text = text
@@ -128,6 +140,7 @@ class Button:
         text_surface = font.render(self.text, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=(self.x + self.width / 2, self.y + self.height / 2))
         screen.blit(text_surface, text_rect)
+
 
 def laser_manage():
     for i in range(len(background_layers)):
@@ -149,6 +162,9 @@ def laser_manage():
         offset = (laser[1].x - player_rect.x, laser[1].y - player_rect.y)
         if player_mask.overlap(laser[2], offset):
             reset_game()
+            return True
+    return False
+
 
 # Function to display menu
 def display_menu():
@@ -157,6 +173,7 @@ def display_menu():
     quit_button = Button("Quit", 860, 650, 200, 100, (255, 0, 0), (200, 0, 0), "quit")
     while running:
         screen.fill((0, 0, 0))
+        screen.blit(background_menu, (0, 0))
         start_button.draw(screen)
         quit_button.draw(screen)
         pygame.display.flip()
@@ -170,17 +187,58 @@ def display_menu():
                 if quit_button.x < mouse_pos[0] < quit_button.x + quit_button.width and quit_button.y < mouse_pos[1] < quit_button.y + quit_button.height:
                     pygame.quit()
                     sys.exit()
+    return 1
+
+
+def display_pause():
+    screen.fill((0, 0, 0))
+
+    for i in range(len(background_layers)):
+        screen.blit(background_layers[i], (layer_positions[i], 0))
+        screen.blit(background_layers[i], (layer_positions[i] + 1920, 0))
+    
+    if player_velocity[0] < 0 or player_rect.bottom < 1020:
+        screen.blit(frames_air[current_frame[0]], player_rect.topleft)
+    else:
+        screen.blit(frames_ground[current_frame[0]], player_rect.topleft)
+
+    for laser in lasers:
+        screen.blit(laser[0], laser[1].topleft)
+
+    score_text = font.render(f'Score: {score[0]}', True, (255, 255, 255))
+    screen.blit(score_text, (1600, 50))
+    score_text = font.render("PAUSE", True, (255, 255, 255))
+    screen.blit(score_text, (960, 500))
+    score_resume = font.render("Press any key to resume", True, (255, 255, 255))
+    screen.blit(score_resume, (800, 600))
+    screen.blit(background_pause, (0, 0))
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+
 # Main game loop
 def game_loop():
-    running = True
+    running = 1
+    pause = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                sys.exit()
+            tmp = 0
+            if event.type == pygame.KEYDOWN and pause == 1:
+                pause = 0
+                tmp = 1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE and tmp == 0:
+                pause = not pause
         
         # Player management
         keys = pygame.key.get_pressed()
         
+        if pause == 1:
+            display_pause()
+            continue
         if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
             player_velocity[0] = boost[0]
         
@@ -199,7 +257,9 @@ def game_loop():
             current_frame[0] = (current_frame[0] + 1) % NUM_FRAMES
             frame_count[0] = 0
         
-        laser_manage()
+        if (laser_manage() == True):
+            running = 0
+            break
 
         score[0] += 1
 
@@ -214,10 +274,18 @@ def game_loop():
 
         # Display the game
         display(player_rect, frames_ground, frames_air, background_layers, layer_positions, lasers, score)
+    return running
 
 def main():
-    display_menu()
-    game_loop()
+    running = 1
+    state = 0
+    while running:
+        if (state == 0):
+            state = display_menu()
+        if (state == 1):
+            state = game_loop()
+        if (state == -1):
+            running = 0
     pygame.quit()
     sys.exit()
 
